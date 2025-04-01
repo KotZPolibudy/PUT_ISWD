4523aad1ded7aa2f214b034e2a6960a754b80071
oskkil <oskkil@onet.pl>
2025-04-01 14:33:08 +0200

    First semi-working example

--- projekt1/src/main.py
+++ projekt1/src/main.py
@@ -7,145 +7,107 @@ R1 = [1,2,3,10,11,12,19,20,21]
 R2 = [4,5,6,13,14,15,22,23,24]
 R3 = [7,8,9,16,17,18,25,26,27]
 
-S1 = [i for i in range(1,10)]
-S2 = [i for i in range(10,19)]
-S3 = [i for i in range(19,28)]
+S1 = list(range(1,10))
+S2 = list(range(10,19))
+S3 = list(range(19,28))
 
-F1 = [i for i in range(1,28) if i%3 == 1]
-F2 = [i for i in range(1,28) if i%3 == 2]
-F3 = [i for i in range(1,28) if i%3 == 0]
-
+F1 = [i for i in range(1, 28) if i%3 == 1]
+F2 = [i for i in range(1, 28) if i%3 == 2]
+F3 = [i for i in range(1, 28) if i%3 == 0]
 
 def read_data(file_path: str) -> list[str]:
     with open(file_path, 'r') as file:
-        data = file.readlines()
-    return data
+        return file.readlines()
 
-def process_data(data: list[str], k: int = 10) -> tuple[list[str], dict[str, list[float]], list[str]]:
-    data = [line.replace('\n', '') for line in data]
-    data = [line.split(',') for line in data]
+def process_data(data: list[str]) -> tuple[list[str], dict[str, list[float]], list[list[str]]]:
+    data = [line.strip().split(',') for line in data]
     criterions = data[0][1:]
     alternatives = data[1:]
-    # alternatives = random.sample(alternatives, k)
-    data = data[1:]
-    data = [line[1:] for line in data]
-    data = np.array(data).T
+    data = np.array([line[1:] for line in alternatives]).T
     data = {criterion: list(map(float, values)) for criterion, values in zip(criterions, data)}
     return criterions, data, alternatives
 
-def get_data(file_path: str) -> tuple[list[str], dict[str, list[float]], list[str]]:
+def get_data(file_path: str) -> tuple[list[str], dict[str, list[float]], list[list[str]]]:
     return process_data(read_data(file_path))
 
 def get_extremes(data: dict[str, list[float]]) -> dict[str, tuple[float, float]]:
     return {criterion: (min(values), max(values)) for criterion, values in data.items()}
 
-def prepare_alternatives(alternatives: list[str], criterions: list[str]):
-    output = {}
-    for i in range(len(alternatives)):
-        output[f"Alternative_{alternatives[i][0]}"] = {criterion: value for criterion, value in zip(criterions, alternatives[i][1:])}
-    return output
-    
-def create_problem(data: dict[str, list[float]], alternatives: list[dict[str, float]]) -> LpProblem:
-    prob = LpProblem("Simplified_UTA_Method", LpMaximize)
+def prepare_alternatives(alternatives: list[list[str]], criterions: list[str]) -> dict[str, dict[str, str]]:
+    return {
+        f"Alternative_{alt[0]}": {criterion: value for criterion, value in zip(criterions, alt[1:])}
+        for alt in alternatives
+    }
 
-    criterion1_values = sorted(list(set(data['C1'])))
-    criterion2_values = sorted(list(set(data['C2'])))
-    criterion3_values = sorted(list(set(data['C3'])))
-    criterion4_values = sorted(list(set(data['C4'])))
+def create_problem(data: dict[str, list[float]], alternatives: dict[str, dict[str, float]], reference_pairs) -> tuple[LpProblem, dict[str, dict[str, LpVariable]]]:
+    prob = LpProblem("UTA_Method", LpMaximize)
 
-    criterion1_vars = {val: LpVariable(f'c1_{val}', 0, 1, cat='Continuous') for val in criterion1_values}
-    criterion2_vars = {val: LpVariable(f'c2_{val}', 0, 1, cat='Continuous') for val in criterion2_values}
-    criterion3_vars = {val: LpVariable(f'c3_{val}', 0, 1, cat='Continuous') for val in criterion3_values}
-    criterion4_vars = {val: LpVariable(f'c4_{val}', 0, 1, cat='Continuous') for val in criterion4_values}
-    
+    criterion_vars = {c: {v: LpVariable(f"{c}_{v}", 0, 1) for v in sorted(set(data[c]))} for c in data}
     extremes = get_extremes(data)
-    minC1, maxC1 = extremes['C1']
-    minC2, maxC2 = extremes['C2']
-    minC3, maxC3 = extremes['C3']
-    minC4, maxC4 = extremes['C4']
-
-    for i in range(len(criterion1_values) - 1):
-        prob += criterion1_vars[criterion1_values[i]] <= criterion1_vars[criterion1_values[i + 1]], f'Monotonicity_C1_{i}'
-    for i in range(len(criterion2_values) - 1):
-        prob += criterion2_vars[criterion2_values[i]] <= criterion2_vars[criterion2_values[i + 1]], f'Monotonicity_C2_{i}'
-    for i in range(len(criterion3_values) - 1):
-        prob += criterion3_vars[criterion3_values[i]] <= criterion3_vars[criterion3_values[i + 1]], f'Monotonicity_C3_{i}'
-    for i in range(len(criterion4_values) - 1):
-        prob += criterion4_vars[criterion4_values[i]] <= criterion4_vars[criterion4_values[i + 1]], f'Monotonicity_C4_{i}'
-    
-    prob += criterion1_vars[minC1] == 0.1, "Normalization_C1_Lowest"
-    prob += criterion2_vars[minC2] == 0.1, "Normalization_C2_Lowest"
-    prob += criterion3_vars[minC3] == 0.1, "Normalization_C3_Lowest"
-    prob += criterion4_vars[minC4] == 0.1, "Normalization_C4_Lowest"
-    prob += criterion1_vars[maxC1] + criterion2_vars[maxC2] + criterion3_vars[maxC3] + criterion4_vars[maxC4] == 1, "Normalization_Criteria_Sum"
-
-    prob += criterion1_vars[maxC1] <= 0.75, "Weight_C1_Limit_Up"
-    prob += criterion2_vars[maxC2] <= 0.75, "Weight_C2_Limit_Up"
-    prob += criterion3_vars[maxC3] <= 0.75, "Weight_C3_Limit_Up"
-    prob += criterion4_vars[maxC4] <= 0.75, "Weight_C4_Limit_Up"
-
-    prob += criterion1_vars[minC1] >= 0.1, "Weight_C1_Limit_Down"
-    prob += criterion2_vars[minC2] >= 0.1, "Weight_C2_Limit_Down"
-    prob += criterion3_vars[minC3] >= 0.1, "Weight_C3_Limit_Down"
-    prob += criterion4_vars[minC4] >= 0.1, "Weight_C4_Limit_Down"
+    for c in data:
+        values = sorted(set(data[c]))
+        for i in range(len(values) - 1):
+            prob += criterion_vars[c][values[i]] <= criterion_vars[c][values[i + 1]]
+        min_val, max_val = extremes[c]
+        prob += criterion_vars[c][min_val] == 0.1
+        prob += criterion_vars[c][max_val] <= 0.75
+    prob += lpSum(criterion_vars[c][max_val] for c, (_, max_val) in extremes.items()) == 1    
 
     alternative_utilities = {}
     for name, evals in alternatives.items():
         utility_var = LpVariable(f"{name}_Utility", lowBound=0)
         alternative_utilities[name] = utility_var
-        prob += utility_var == lpSum(
-            [
-                criterion1_vars[float(evals['C1'])],
-                criterion2_vars[float(evals['C2'])],
-                criterion3_vars[float(evals['C3'])],
-                criterion4_vars[float(evals['C4'])]
-            ]
-        ), f"Utility_{name}"
+        prob += utility_var == lpSum(criterion_vars[c][float(evals[c])] for c in data)
+
     epsilon = LpVariable("epsilon", lowBound=0)
-    # prob += alternative_utilities['Alternative_2'] >= alternative_utilities['Alternative_1'] + epsilon, "Pref_A2_A1"
-    # prob += alternative_utilities['Alternative_1'] >= alternative_utilities['Alternative_3'] + epsilon, "Pref_A1_A3"
-    
-    # prob += criterion1_vars[float(alternatives['Alternative_2']['C1'])] >= criterion1_vars[float(alternatives['Alternative_1']['C1'])] + epsilon, "Pref_A2_A1"
-    # prob += criterion1_vars[float(alternatives['Alternative_1']['C1'])] >= criterion1_vars[float(alternatives['Alternative_3']['C1'])] + epsilon, "Pref_A1_A3"
+    prob += epsilon
+    for a1, a2 in reference_pairs:
+        prob += alternative_utilities[a2] >= alternative_utilities[a1] + epsilon
 
-    prob += epsilon, "Maximize_Epsilon"
-    prob.sense = LpMaximize
-    return prob, criterion1_values, criterion1_vars, criterion2_values, criterion2_vars, criterion3_values, criterion3_vars, criterion4_values, criterion4_vars
-
-def plot_results(criterion_values: list[list[float]], criterion_vars: list[dict[str, LpVariable]]):
-    fig, axes = plt.subplots(2, 2, figsize=(10, 8))
-    fig.suptitle('Partial Utility Functions for Each Criterion')
+    # Czwarta grupa: Preferowana jest lokalizacja R2 nad R1 oraz R1 nad R3.
+    for alt2, alt1 in zip(R2, R1):
+        prob += alternative_utilities[f"Alternative_{alt2}"] >= alternative_utilities[f"Alternative_{alt1}"] + epsilon
+    for alt1, alt3 in zip(R1, R3):
+        prob += alternative_utilities[f"Alternative_{alt1}"] >= alternative_utilities[f"Alternative_{alt3}"] + epsilon
     
-    for idx, (criterion_values, criterion_vars) in enumerate(zip(criterion_values, criterion_vars)):
-        row, col = divmod(idx, 2)
-        solver_values = [value(criterion_vars[val]) for val in criterion_values]
+    # Trzecia grupa: Inwestorzy chcą jak najdłuzej utrzymywać składowisko.    
+    # preferowane opcje ze scenariusza czasowego S3 nad S1 oraz S2
+    for alt3, alt12 in zip(S3, S1 + S2):
+        prob += alternative_utilities[f"Alternative_{alt3}"] >= alternative_utilities[f"Alternative_{alt12}"] + epsilon
         
-        axes[row, col].plot(criterion_values, solver_values, marker='o', linestyle='-', label=f'$u(g_{{C_{idx+1}}})$')
-        axes[row, col].set_xlabel(f'$g_{{C_{idx+1}}}$')
-        axes[row, col].set_ylabel(f'$u(g_{{C_{idx+1}}})$')
-        axes[row, col].set_title(f'Criterion $C_{idx+1}$')
-        axes[row, col].grid(True)
+    return prob, criterion_vars
+
+def select_reference_pairs(alternatives: dict[str, dict[str, str]], k=5) -> list[tuple[str, str]]:
+    alt_keys = list(alternatives.keys())
+    return random.sample([(a1, a2) for a1 in alt_keys for a2 in alt_keys if a1 != a2], k)
+
+def plot_results(criterion_vars):
+    fig, axes = plt.subplots(2, 2, figsize=(10, 8))
+    for idx, (c, vars) in enumerate(criterion_vars.items()):
+        row, col = divmod(idx, 2)
+        x = sorted(vars.keys())
+        y = [value(vars[v]) for v in x]
+        axes[row, col].plot(x, y, marker='o', linestyle='-', label=f'$u(g_{{{c}}})$')
+        axes[row, col].set_title(f'Criterion {c}')
         axes[row, col].legend()
-    
-    plt.tight_layout(rect=[0, 0, 1, 0.96])
+    plt.tight_layout()
     plt.show()
 
 if __name__ == '__main__':
     criterions, values, alternatives = get_data('data.csv')
     alternatives = prepare_alternatives(alternatives, criterions)
-    prob, criterion1_values, criterion1_vars, criterion2_values, criterion2_vars, criterion3_values, criterion3_vars, criterion4_values, criterion4_vars = create_problem(values, alternatives)
+    reference_pairs = select_reference_pairs(alternatives)
+    
+    prob, criterion_vars = create_problem(values, alternatives, reference_pairs)
     prob.solve()
 
     print(f"Status: {LpStatus[prob.status]}")
-    print("\nOptimal Solution:")
-    for var in prob.variables():
-        print(f"{var.name} = {value(var)}")
-
+    # for var in prob.variables():
+    #     print(f"{var.name} = {value(var)}")
     print("\nObjective value:", value(prob.objective))
 
-    plot_results(
-        [criterion1_values, criterion2_values, criterion3_values, criterion4_values], 
-        [criterion1_vars, criterion2_vars, criterion3_vars, criterion4_vars]
-    )
-
+    plot_results(criterion_vars)
     
+    utilities = {var.name[:-8]: value(var) for var in prob.variables() if var.name.endswith("_Utility")}
+    for a1, a2 in reference_pairs:
+        print(f"Utility of {a1} >= Utility of {a2}: {utilities[a1]} >= {utilities[a2]}")
